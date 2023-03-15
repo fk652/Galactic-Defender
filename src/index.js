348a01ee30c9dc9f8800b974dec3ef0b1ca80688
Fahim Khan <fk652@nyu.edu>
2023-03-15 14:14:14 -0400

    fix for window load

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import GameView from "./scripts/game_view";
 
-window.canvas = document.getElementById("game-view");
-window.ctx = canvas.getContext("2d");
-canvas.width = 700;
-canvas.height = 550;
-
-const view = new GameView(canvas, ctx);
-
-window.onload = () => view.start();
+window.addEventListener("load", () => {
+  window.canvas = document.getElementById("game-view");
+  window.ctx = canvas.getContext("2d");
+  canvas.width = 700;
+  canvas.height = 550;
+  const view = new GameView(canvas, ctx);
+  view.start()
+});
