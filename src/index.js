30c376f1aae7e8ff5e609c8e6ab2c1065f637cd8
Fahim Khan <fk652@nyu.edu>
2023-05-17 16:24:17 -0400

    update canvas css

--- src/index.js
+++ src/index.js
@@ -44,8 +44,6 @@ soundsUrls.forEach((url, i) => {
 window.addEventListener("load", () => {
   window.canvas = document.getElementById("game-view");
   window.ctx = canvas.getContext("2d");
-  canvas.width = 700;
-  canvas.height = 550;
   const view = new GameView(canvas, ctx);
   view.start()
 });
